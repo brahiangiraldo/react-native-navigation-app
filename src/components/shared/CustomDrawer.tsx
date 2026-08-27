a8c37fe9608c8d0ced6a2d2cce490daca97b8d24
Brahian <brahian.giraldog@gmail.com>
2026-08-27 16:21:02 -0500

    feat: change in config of taildwind and class

--- src/components/shared/CustomDrawer.tsx
+++ src/components/shared/CustomDrawer.tsx
@@ -1,17 +1,22 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-import { DrawerContentScrollView } from 'expo-router/drawer'
+import {
+    DrawerContentScrollView,
+    DrawerItemList,
+    type DrawerContentComponentProps,
+} from 'expo-router/drawer'
 
-const CustomDrawer = () => {
+const CustomDrawer = (props: DrawerContentComponentProps) => {
     return (
-        <DrawerContentScrollView>
+        <DrawerContentScrollView {...props}>
             <View className="flex justify-center items-center mx-3 p-10 mb-10 h-[150px] rounded-xl bg-primary">
                 <View className="flex justify-center items-center bg-white rounded-full h-24 w-24">
                     <Text className="text-3xl font-bold text-primary font-work-black">BMG</Text>
                 </View>
             </View>
+
+            <DrawerItemList {...props} />
         </DrawerContentScrollView>
     )
 }
 
-export default CustomDrawer;
+export default CustomDrawer
